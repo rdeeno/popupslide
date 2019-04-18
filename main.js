b850923169aa349a97de2eb35603d68816167fe6
Denis <d.russu@webus.md>
2019-04-18 12:14:49 +0300

    main js

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
-var imageGrid = document.querySelector(".image__grid");
-var overlay = document.querySelector(".overlay");
-var closeBtn = document.querySelector(".closeBtn");
-var slideWrapper = document.querySelector(".sliderWrapper");
-var elements = [...document.querySelectorAll(".img img")];
-var next = document.querySelector(".right");
-var prev = document.querySelector(".left");
-var slide = [...document.querySelectorAll(".slide")];
-let current = 0;
-var clickedImg;
-next.addEventListener("click", function () {
-    slideChange('next')
-});
-prev.addEventListener("click", function () {
-    slideChange('prev')
-});
-overlay.addEventListener("click", closeHelpers)
-closeBtn.addEventListener("click", closeHelpers)
-imageGrid.addEventListener("click", function (e) {
-    e.stopPropagation();
-    var target = e.target;
-    clickedImg = elements.indexOf(target);
-    var checkIfImg = target.closest("img");
-    if (!checkIfImg) return;
-    overlay.style.transform = "translateY(0)";
-    slideWrapper.classList.add("visible");
-    slide[clickedImg].classList.add("active");
-    current = clickedImg;
-});
-
-function slideChange(dir) {
-    slide[current].classList.remove("active");
-    slide[clickedImg].classList.remove("active");
-    current = dir === 'next' ? current += 1 : current -= 1;
-    if (current >= slide.length) {
-        current = 0;
-    } else if (current < 0) {
-        current = slide.length - 1;
+class modalSlide {
+    constructor() {
+        this.imageGrid = document.querySelector(".image__grid");
+        this.overlay = document.querySelector(".overlay");
+        this.closeBtn = document.querySelector(".closeBtn");
+        this.slideWrapper = document.querySelector(".sliderWrapper");
+        this.elements = [...document.querySelectorAll(".img img")];
+        this.next = document.querySelector(".right");
+        this.prev = document.querySelector(".left");
+        this.slide = [...document.querySelectorAll(".slide")];
+        this.current = 0;
+        this.clickedImg;
+        this.imageGrid.addEventListener("click", this.getElements.bind(this));
+        this.next.addEventListener("click", this.slideChange.bind(this, 'next'));
+        this.prev.addEventListener("click", this.slideChange.bind(this, 'prev'));
+        this.overlay.addEventListener("click", this.closeHelpers.bind(this));
+        this.closeBtn.addEventListener("click", this.closeHelpers.bind(this))
     }
-    slide[current].classList.add("active");
+    getElements(e) {
+        e.stopPropagation();
+        var target = e.target;
+        this.clickedImg = this.elements.indexOf(target);
+        var checkIfImg = target.closest("img");
+        if (!checkIfImg) return;
+        this.overlay.style.transform = "translateY(0)";
+        this.slideWrapper.classList.add("visible");
+        this.slide[this.clickedImg].classList.add("active");
+        this.current = this.clickedImg;
+    }
+    slideChange(dir) {
+        this.slide[this.current].classList.remove("active");
+        this.slide[this.clickedImg].classList.remove("active");
+        this.current = dir === 'next' ? this.current += 1 : this.current -= 1;
+        if (this.current >= this.slide.length) {
+            this.current = 0;
+        } else if (this.current < 0) {
+            this.current = this.slide.length - 1;
+        }
+        this.slide[this.current].classList.add("active");
+    }
+    closeHelpers() {
+        this.overlay.style.transform = "translateY(-100%)";
+        this.slideWrapper.classList.remove("visible");
+        this.slide[this.current].classList.remove("active");
+    }
 }
-
-function closeHelpers() {
-    overlay.style.transform = "translateY(-100%)";
-    slideWrapper.classList.remove("visible");
-    slide[current].classList.remove("active");
-}
+new modalSlide()
