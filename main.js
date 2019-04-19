74dc8a912ee48551c42438404c02aacacbc4018e
Denis <d.russu@webus.md>
2019-04-19 19:15:22 +0300

    iframe added

--- main.js
+++ main.js
@@ -10,8 +10,10 @@ class modalSlide {
         this.slideWrapperElms = this.slideWrapper.querySelectorAll(".slide");
         this.overlay = document.querySelector(".overlay");
         this.closeBtn = document.querySelector(".closeBtn");
+        this.helper = document.querySelector('.helper')
         this.nextBtn = document.querySelector(".right");
         this.prevBtn = document.querySelector(".left");
+        this.iframeContent = document.querySelector('#video');
         this.current = 0;
         this.clickedImg;
         this.imageGrid.addEventListener("click", this.getElements.bind(this));
@@ -19,37 +21,45 @@ class modalSlide {
         this.prevBtn.addEventListener("click", this.slideChange.bind(this, 'prev'));
         this.overlay.addEventListener("click", this.closeHelpers.bind(this));
         this.closeBtn.addEventListener("click", this.closeHelpers.bind(this))
+
+
     }
     getElements(e) {
         e.stopPropagation();
         var target = e.target;
-        this.clickedImg = this.imageGridElms.indexOf(target);
-        var checkIfImg = target.closest("img");
+        var checkIfImg = target.closest("div");
         if (!checkIfImg) return;
-        document.querySelector('.helper').classList.add('visible');
-
+        this.clickedImg = this.imageGridElms.indexOf(target);
+        this.helper.classList.add('visible');
         this.slideWrapper.classList.add("visible");
-
         this.slideWrapperElms[this.clickedImg].classList.add("active");
         this.current = this.clickedImg;
+        if (this.slideWrapperElms[this.current].querySelector('iframe')) {
+            this.iframeContent.contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
+        }
+        console.log(this.slideWrapperElms[this.clickedImg].querySelector('iframe'));
+        console.log(this.current);
     }
     slideChange(dir) {
         this.slideWrapperElms[this.current].classList.remove("active");
-        this.slideWrapperElms[this.clickedImg].classList.remove("active");
         this.current = dir === 'next' ? this.current += 1 : this.current -= 1;
+
+        this.iframeContent.contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
         if (this.current >= this.slideWrapperElms.length) {
             this.current = 0;
         } else if (this.current < 0) {
             this.current = this.slideWrapperElms.length - 1;
         }
+        if (this.slideWrapperElms[this.current].querySelector('iframe')) {
+            this.iframeContent.contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
+        }
         this.slideWrapperElms[this.current].classList.add("active");
     }
     closeHelpers() {
-        document.querySelector('.helper').classList.remove('visible');
-
+        this.helper.classList.remove('visible');
         this.slideWrapper.classList.remove("visible");
-
         this.slideWrapperElms[this.current].classList.remove("active");
+        this.iframeContent.contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
     }
 }
 
