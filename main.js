4f426e04509edf77c1a91b0aef5f4397c221e5fc
Denis <d.russu@webus.md>
2019-04-19 12:54:35 +0300

    mod js

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 class modalSlide {
     constructor(selector, options) {
         var defaults = {
-            slideContainer: '.slideWrapper'
+            slideContainer: '.con'
         }
         this.params = Object.assign({}, defaults, options)
         this.imageGrid = selector;
         this.imageGridElms = [...this.imageGrid.querySelectorAll(".img img")];
-        this.slideWrapper = document.querySelectorAll(this.params.slideContainer); /*ВОТ ЗДЕСЬ*/
-        this.slideWrapperElms = [...document.querySelectorAll(".slide")];
+        this.slideWrapper = document.querySelector(this.params.slideContainer);
+        this.slideWrapperElms = this.slideWrapper.querySelectorAll(".slide");
         this.overlay = document.querySelector(".overlay");
         this.closeBtn = document.querySelector(".closeBtn");
         this.nextBtn = document.querySelector(".right");
         this.prevBtn = document.querySelector(".left");
-
         this.current = 0;
         this.clickedImg;
         this.imageGrid.addEventListener("click", this.getElements.bind(this));
@@ -20,7 +19,6 @@ class modalSlide {
         this.prevBtn.addEventListener("click", this.slideChange.bind(this, 'prev'));
         this.overlay.addEventListener("click", this.closeHelpers.bind(this));
         this.closeBtn.addEventListener("click", this.closeHelpers.bind(this))
-        console.log(this.slideWrapper);
     }
     getElements(e) {
         e.stopPropagation();
@@ -29,7 +27,9 @@ class modalSlide {
         var checkIfImg = target.closest("img");
         if (!checkIfImg) return;
         document.querySelector('.helper').classList.add('visible');
+
         this.slideWrapper.classList.add("visible");
+
         this.slideWrapperElms[this.clickedImg].classList.add("active");
         this.current = this.clickedImg;
     }
@@ -46,15 +46,18 @@ class modalSlide {
     }
     closeHelpers() {
         document.querySelector('.helper').classList.remove('visible');
+
         this.slideWrapper.classList.remove("visible");
+
         this.slideWrapperElms[this.current].classList.remove("active");
     }
 }
-var item = document.querySelector('.image__grid')
-var itemSecond = document.querySelector('.image__grid-second')
+
+var item = document.querySelector('.imgGrd')
+var itemSecond = document.querySelector('.imgGrd-second')
 new modalSlide(item, {
-    slideContainer: '.slideWrapper'
+    slideContainer: '.sldWrp'
 })
 new modalSlide(itemSecond, {
-    slideContainer: '.slideWrapper-second'
+    slideContainer: '.sldWrp-second'
 })
